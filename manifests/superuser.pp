c67c1f1088197cdf1d0261e311f179844f38662e
primeministerp <primeministerpete@gmail.com>
2015-05-11 12:34:15 -0400

    trying to fix superuser

--- manifests/superuser.pp
+++ manifests/superuser.pp
@@ -56,13 +56,14 @@ define maas::superuser ( $superuser_name = $name, $password, $email ) {
       logoutput   => true,
       before      => Exec["logout-superuser-with-api-key-$name"],
       require     => Exec["login-superuser-with-api-key-$name"],
-    }
+    } ->
+
     exec{'maas-boot-resources-import-run-by-user-$name':
       command     => "/usr/bin/maas ${maas::profile_name} boot-resources import",
       cwd         => '/etc/maas/.puppet',
       logoutput   => true,
       before      => Exec["logout-superuser-with-api-key-$name"],
-      require     => Exec["login-superuser-with-api-key-$name","maas-import-boot-images-run-by-user-$name"],
+      require     => Exec["login-superuser-with-api-key-$name"],
     }
   }
 

f50ff906cdd969e1935df33a2b689630670d60b8
primeministerp <primeministerpete@gmail.com>
2015-05-11 12:20:49 -0400

    trying to fix superuser tasks

--- manifests/superuser.pp
+++ manifests/superuser.pp
@@ -57,6 +57,13 @@ define maas::superuser ( $superuser_name = $name, $password, $email ) {
       before      => Exec["logout-superuser-with-api-key-$name"],
       require     => Exec["login-superuser-with-api-key-$name"],
     }
+    exec{'maas-boot-resources-import-run-by-user-$name':
+      command     => "/usr/bin/maas ${maas::profile_name} boot-resources import",
+      cwd         => '/etc/maas/.puppet',
+      logoutput   => true,
+      before      => Exec["logout-superuser-with-api-key-$name"],
+      require     => Exec["login-superuser-with-api-key-$name"],
+    }
   }
 
   ## Command to Log out profile and flush creds

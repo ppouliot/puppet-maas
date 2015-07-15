e0dd39c8ab238f7d2e58ead499f4f2d8f866b967
primeministerp <primeministerpete@gmail.com>
2015-07-15 15:49:36 -0400

    updating cluster controller with same as current node def

--- manifests/cluster_controller.pp
+++ manifests/cluster_controller.pp
@@ -12,23 +12,38 @@
 #
 class maas::cluster_controller (
 
-  $maas_cluster_controller_config_file   = $maas::params::maas_cluster_controller_config_file,
-  $maas_cluster_controller_initi_file    = $maas::params::maas_cluster_controller_initi_file,
-  $maas_cluster_controller_frontend_port = $maas::params::maas_cluster_controller_frontend_port,
-  $maas_cluster_controller_prefix        = $maas::params::maas_cluster_controller_prefix, 
-  $maas_cluster_controller_oops_dir      = $maas::params::maas_cluster_controller_oops_dir, 
-  $maas_cluster_controller_oops_reporter = $maas::params::maas_cluster_controller_oops_reporter, 
-  $maas_cluster_controller_broker_host   = $maas::params::maas_cluster_controller_broker_host, 
-  $maas_cluster_controller_broker_port   = $maas::params::maas_cluster_controller_broker_port,
-  $maas_cluster_controller_broker_user   = $maas::params::maas_cluster_controller_broker_user,
-  $maas_cluster_controller_broker_passwd = $maas::params::maas_cluster_controller_broker_passwd,
-  $maas_cluster_controller_broker_vhost  = $maas::params::maas_cluster_controller_broker_vhost,
-  $maas_cluster_controller_logfile       = $maas::params::maas_cluster_controller_logfile,
+#  $maas_cluster_controller_config_file   = $maas::params::maas_cluster_controller_config_file,
+#  $maas_cluster_controller_initi_file    = $maas::params::maas_cluster_controller_initi_file,
+#  $maas_cluster_controller_frontend_port = $maas::params::maas_cluster_controller_frontend_port,
+#  $maas_cluster_controller_prefix        = $maas::params::maas_cluster_controller_prefix, 
+#  $maas_cluster_controller_oops_dir      = $maas::params::maas_cluster_controller_oops_dir, 
+#  $maas_cluster_controller_oops_reporter = $maas::params::maas_cluster_controller_oops_reporter, 
+#  $maas_cluster_controller_broker_host   = $maas::params::maas_cluster_controller_broker_host, 
+#  $maas_cluster_controller_broker_port   = $maas::params::maas_cluster_controller_broker_port,
+#  $maas_cluster_controller_broker_user   = $maas::params::maas_cluster_controller_broker_user,
+#  $maas_cluster_controller_broker_passwd = $maas::params::maas_cluster_controller_broker_passwd,
+#  $maas_cluster_controller_broker_vhost  = $maas::params::maas_cluster_controller_broker_vhost,
+#  $maas_cluster_controller_logfile       = $maas::params::maas_cluster_controller_logfile,
 
 
 ) inherits maas::params {
 
+ # The following is essentially what I used in the node definition to create a secondary cluster controller
+ # There is an additional template needed.
 
+  include apt
+  apt::ppa{"ppa:maas-maintainers/stable":} ->
+  package{'maas-cluster-controller':
+    ensure => latest,
+  } ->
+  file{'/var/lib/maas/secret':
+    ensure => file,
+    owner  => 'maas',
+    group  => 'maas',
+    mode   => '640',
+    source => 'puppet:///extra_files/maas/secret',
+  }
+#}
 ## /etc/maas/maas_cluster.yaml
 ## /etc/maas/maas-cluster-http.conf
 ## /etc/maas/maas-http.conf
@@ -37,33 +52,33 @@ class maas::cluster_controller (
 
   ## /etc/maas/maas_cluster.yaml
 
-  file{ '/etc/maas/maas_cluster.yaml':
-    ensure => present,
-    content => template('maas/maas_cluster.yaml.erb')
-  }
+#  file{ '/etc/maas/maas_cluster.yaml':
+#    ensure => present,
+#    content => template('maas/maas_cluster.yaml.erb')
+#  }
 
   ## /etc/maas/maas-cluster-http.conf
-  file{ '/etc/maas/maas-cluster-http.conf':
-    ensure => present,
-    content => template('maas/maas-cluster-http.conf.erb')
-  }
+#  file{ '/etc/maas/maas-cluster-http.conf':
+#    ensure => present,
+#    content => template('maas/maas-cluster-http.conf.erb')
+#  }
 
   ## /etc/maas/maas-http.conf
-  file{ '/etc/maas/maas-http.conf':
-    ensure => present,
-    content => template('maas/maas-http.conf.erb')
-  }
+#  file{ '/etc/maas/maas-http.conf':
+#    ensure => present,
+#    content => template('maas/maas-http.conf.erb')
+#  }
 
   ## /etc/maas/maas_local_celeryconfig.py
-  file{ '/etc/maas/maas_local_celeryconfig.py':
-    ensure => present,
-    content => template('maas/maas_local_celeryconfig.py.erb')
-  }
+#  file{ '/etc/maas/maas_local_celeryconfig.py':
+#    ensure => present,
+#    content => template('maas/maas_local_celeryconfig.py.erb')
+#  }
 
   ## /etc/maas/maas_local_celeryconfig_cluster.py
-  file{ '/etc/maas/maas_local_celeryconfig_cluster.py':
-    ensure => present,
-    content => template('maas/maas_local_celeryconfig_cluster.py.erb')
-  }
+#  file{ '/etc/maas/maas_local_celeryconfig_cluster.py':
+#    ensure => present,
+#    content => template('maas/maas_local_celeryconfig_cluster.py.erb')
+#  }
 
 }

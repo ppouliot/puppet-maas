af27889c6abe4669d263cece44a50d43a2edf649
primeministerp <primeministerpete@gmail.com>
2015-08-21 15:45:31 -0400

    removing exec of centos images for now

--- manifests/image_builder.pp
+++ manifests/image_builder.pp
@@ -22,22 +22,22 @@ class maas::image_builder (
     cwd       => '/opt/maas-image-builder',
     logoutput => true,
     timeout   => 0,
-    notify    => Exec['create-centos-7-image','create-centos-6-image'],
+#    notify    => Exec['create-centos-7-image','create-centos-6-image'],
   } ->
   exec {'create-centos-7-image':
-    command   => '/opt/maas-image-builder/bin/maas-image-builder -o centos7-amd64-root-tgz centos',
-    cwd       => '/opt/maas-image-builder',
-    logoutput => true,
+    command     => '/opt/maas-image-builder/bin/maas-image-builder -o centos7-amd64-root-tgz centos',
+    cwd         => '/opt/maas-image-builder',
+    logoutput   => true,
     refreshonly => true,
-    timeout   => 0,
+    timeout     => 0,
     require     => Exec[image-builder_make_install-deps],
   } ->
   exec {'create-centos-6-image':
-    command   => '/opt/maas-image-builder/bin/maas-image-builder -o centos6-amd64-root-tgz centos --edition 6',
-    cwd       => '/opt/maas-image-builder',
-    logoutput => true,
+    command     => '/opt/maas-image-builder/bin/maas-image-builder -o centos6-amd64-root-tgz centos --edition 6',
+    cwd         => '/opt/maas-image-builder',
+    logoutput   => true,
     refreshonly => true,
-    timeout   => 0,
+    timeout     => 0,
     require     => Exec[image-builder_make_install-deps],
   }
 }

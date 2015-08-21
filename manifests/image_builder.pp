97df7465af86e93311e9f065a3a0cd968d1e4cc1
primeministerp <primeministerpete@gmail.com>
2015-08-21 14:55:03 -0400

    adding image creation process

--- manifests/image_builder.pp
+++ manifests/image_builder.pp
@@ -5,15 +5,33 @@ class maas::image_builder (
 
   package{['bzr',
            'python-tempita',
-           'python-yaml']:
-    ensure   => latest
+           'python-yaml',
+           'python-virtualenv']:
+    ensure    => latest
   } ->
 
   vcsrepo { '/opt/maas-image-builder/':
-    ensure   => latest,
-    provider => bzr,
-    source   => 'lp:maas-image-builder',
+    ensure    => latest,
+    provider  => bzr,
+    source    => 'lp:maas-image-builder',
+  } ->
+
+  exec {'image-builder_make_install-deps':
+    command   => 'make install-dependencies && make',
+    cwd       => '/opt/maas-image-builder',
+    logoutput => true,
+    timeout   => 0,
   }
-
-
+  exec {'create-centos-7-image':
+    command   => './bin/maas-image-builder -o centos7-amd64-root-tgz centos'
+    cwd       => '/opt/maas-image-builder',
+    logoutput => true,
+    timeout   => 0,
+  } ->
+  exec {'create-centos-6-image':
+    command   => './bin/maas-image-builder -o centos6-amd64-root-tgz centos --edition 6',
+    cwd       => '/opt/maas-image-builder',
+    logoutput => true,
+    timeout   => 0,
+  }
 }

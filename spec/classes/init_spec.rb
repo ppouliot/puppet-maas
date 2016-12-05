ca49d63dfa9faaee088ed9d718f69f8229ef1964
Lukas Kallies <kallies@puzzle.ch>
2016-12-05 17:49:41 +0100

    Add init_spec from selyx-maas

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 describe 'maas' do
-
+  let(:facts) do
+    {
+      :operatingsystem        => 'Ubuntu',
+      :operatingsystemrelease => '14.04',
+    }
+  end
   context 'with defaults for all parameters' do
     it { should contain_class('maas') }
   end

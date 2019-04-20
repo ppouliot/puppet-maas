014e9e00cb4db9e8daf113e831a7b3a5d8d70b55
Peter Pouliot <peter@pouliot.net>
2019-04-19 21:26:17 -0400

    removing artifacts

--- files/scripts/create_pod.sh
+++ files/scripts/create_pod.sh
@@ -2,12 +2,17 @@
 MAAS_URL='http://localhost/MAAS/api/2.0'
 POD_TYPE='virsh'
 POD_NAME=$POD_TYPE-$RANDOM
-VIRSH_URL="qemu+ssh://$MAAS_USER@$POWER_ADDRESS/system"
-MAAS_USER='ubuntu'
-POWER_ADDRESS='10.20.20.11'
+PROFILE='admin'
+POWER_ADDRESS='qemu+ssh://ubuntu@10.20.20.11/system'
 POWER_PASS='ubuntu'
+RESOURCE_POOL='maas-ctrl'
 
-maas login $MAAS_USER $MAAS_URL $(maas-region apikey --username=$MAAS_USER)
-maas $MAAS_USER pods create -d type=$POD_TYPE name=$POD_NAME power_address=$POWER_ADDRESS power_pass=$POWER_PASS
+#maas login $PROFILE $MAAS_URL $(maas-region apikey --username=$PROFILE)
 
-POD_ID=$(maas $PROFILE podss read | grep "^\ \ \ \ \ \ \ \ \"system_id\":" | awk '{print $2}' | sed -e ':a;N;$!ba;s/\n/ /g'| sed -e 's|[",]||g')
+maas $PROFILE resource-pools create name=$RESOURCE_POOL description="KVM Resources on MAAS Controller"
+maas $PROFILE pods create -d type=$POD_TYPE name=$POD_NAME power_address=$POWER_ADDRESS power_pass=$POWER_PASS tags="kvm,virtual" pool="$RESOURCE_POOL" cpu_over_commit_ratio=0.0 memory_over_commit_ratio=0.0 default_storage_pool=default
+
+POD_ID=$(maas $PROFILE pods read | grep "^\ \ \ \ \ \ \ \ \"id\":" | awk '{print $2}' | sed -e ':a;N;$!ba;s/\n/ /g'| sed -e 's|[",]||g')
+echo "Pod ID: " $POD_ID
+maas $PROFILE pod compose $POD_ID cores="12" memory="8196" architecture="amd64/generic" hostname="juju1" storage="vm_disk:40(default)"
+
